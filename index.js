73de19b3c353bf1d91d6e104b81511c8fc987a51
GilzenPilzen <gilles.van.biervliet@student.howest.be>
2020-10-13 18:18:55 +0200

    einde van de challenge!!!!!

--- index.js
+++ index.js
@@ -27,14 +27,41 @@ const clients = {};
 io.on('connection', socket => {
   clients[socket.id] = { id: socket.id };
 
+  socket.on('name', name => {
+    console.log('name', name);
+    // check if the name is valid
+    if (!name || !name.trim || name.trim().length === 0) {
+      return;
+    }
+    name = name.trim();
+    for (const socketId in clients) {
+      if (clients.hasOwnProperty(socketId)) {
+        const client = clients[socketId];
+        if (client.name === name) {
+          // name is already taken
+          return;
+        }
+      }
+    }
+    clients[socket.id].name = name;
+    // send the name back - it is ok!
+    socket.emit('name', name);
+  });
+  
+
   socket.on('disconnect', () => {
     delete clients[socket.id];
     io.emit('clients', clients);
   });
 
-  socket.on('signal', (peerId, signal) => {
+  socket.on('signal', (peerId, signal, name) => {
     console.log(`Received signal from ${socket.id} to ${peerId}`);
-    io.to(peerId).emit('signal', peerId, signal, socket.id);
+    io.to(peerId).emit('signal', peerId, signal, socket.id, name);
+  });
+
+  socket.on('openLink', (url, peerId)  => {
+    console.log(url);
+    io.to(peerId).emit('openLink', url, peerId, socket.id);
   });
 
   io.emit('clients', clients);
